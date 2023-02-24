aecaae2a003d06e19872101e8569bf384febbad8
Pragathi Hebbar K M <phebbarkm@BLR.ALLEGISINDIA.COM>
2023-02-24 09:46:45 +0530

    new prg Car

---
+++ JavaNewProject/src/com/demo/Car.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Car {
+
+	String color = "White";
+	public int numOfWheels;
+	private int numOfSeats;
+	public static String manufacturer = "Tata";
+	
+	public Car() {
+		color = "Silver";
+		numOfWheels=4;
+	}
+}
--- JavaNewProject/src/com/demo/StaticAndNonStaticPrg.java
+++ JavaNewProject/src/com/demo/StaticAndNonStaticPrg.java
@@ -5,19 +5,26 @@ public class StaticAndNonStaticPrg {
 	public StaticAndNonStaticPrg() {
 		System.out.println("this is from constructor");
 	}
-public static void main(String[] args) {
-	System.out.println("this is from main method");
-	StaticAndNonStaticPrg obj1=new StaticAndNonStaticPrg();
-	StaticAndNonStaticPrg obj2=new StaticAndNonStaticPrg();
-	StaticAndNonStaticPrg obj3=new StaticAndNonStaticPrg();
-}
+	public static void main(String[] args) {
+		System.out.println("this is from main method");
+		
+		StaticAndNonStaticPrg obj1=new StaticAndNonStaticPrg();
+		StaticAndNonStaticPrg obj2=new StaticAndNonStaticPrg();
+		StaticAndNonStaticPrg obj3=new StaticAndNonStaticPrg();
+		
+		Car mycar = new Car();
+		
+		System.out.println(mycar.numOfWheels);
+		System.out.println(mycar.color);
+		System.out.println(Car.manufacturer);
+	}
 
-static {
-	System.out.println("this is from static block");
-}
+	static {
+		System.out.println("this is from static block");
+	}
 
-{
-	System.out.println("this is from non-static block");
-}
+	{
+		System.out.println("this is from non-static block");
+	}
 
 }
