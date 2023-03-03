48f5becd3dd951b99c71c9440b8f82fa788e9bb8
Pragathi Hebbar K M <phebbarkm@BLR.ALLEGISINDIA.COM>
2023-03-03 09:21:07 +0530

    Thread sleep prg added

--- JavaNewProject/src/com/demo/ThreadDemo.java
+++ JavaNewProject/src/com/demo/ThreadDemo.java
@@ -3,11 +3,13 @@ package com.demo;
 public class ThreadDemo {
 
 	public static void main(String[] args) {
-		SingleThreaded st = new SingleThreaded();
-		st.printNum();
 		
 		MultiThreaded mt = new MultiThreaded();
 		mt.start();
+		
+		SingleThreaded st = new SingleThreaded();
+		st.printNum();
+		
 	}
 }
 
@@ -26,6 +28,12 @@ class MultiThreaded extends Thread {
 	public void run() {
 		for(int i = 1; i<=200; i++) {
 			System.out.println(Thread.currentThread().getName()+ "|" +i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
